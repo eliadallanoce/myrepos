c85a56bbe19dc0f38a0891e6588edd8124d2d081
eliadallanoce <59958880+eliadallanoce@users.noreply.github.com>
2020-05-21 15:46:09 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
 const express= require('express');
 const app= express();
 var port = process.env.PORT || 3000;
-var persone = require("./persone.json");
 app.get('/', (req, res)=> res.send("Ciao Mondo!!!"));
-app.route('/persone')
-    .get(function (req, res) {
-        persone.forEach(pers => {
-            res.send("Nome:" + pers.Nome + " Cognome:" + pers.Cognome + " Età:" + pers.Eta);
-        });
-
-    });
 app.listen(port, ()=> console.log('server in ascolto:'+ port));
